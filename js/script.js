6c6ea4e3fe8f7d5cb1e64f3240ccd19211517a35
rfaeroe <57751702+rfaeroe@users.noreply.github.com>
2020-10-16 15:15:11 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+document.querySelectorAll("[id^=routeline_]")
+    .forEach(function (routeline) {
+        routeline.addEventListener("mouseover", function (event) {
+
+            // move this line to the front
+            event.currentTarget.parentElement.append(event.currentTarget);
+        });
+    });
+
+
+
+
+
 document.querySelectorAll('[id^="terminal_"]')
     .forEach(function (terminalElem) {
         console.log(terminalElem.id, terminalElem)
@@ -11,12 +24,39 @@ document.querySelectorAll('[id^="terminal_"]')
 
         // set up hover events
         terminalElem.addEventListener("mouseover", function (event) {
-            console.log("hovered");
             popupElem.classList.add("popup-open");
         });
         terminalElem.addEventListener("mouseout", function (event) {
-            console.log("unhovered");
             popupElem.classList.remove("popup-open");
         });
+
+    });
+
+
+document.querySelectorAll('[id^="routestop_"]')
+    .forEach(function (routeStopElem) {
+        routeStopElem.addEventListener("moseover", function (event) {
+            // move this line to the front
+            event.currentTarget.parentElement.append(event.currentTarget);
+        })
+
+
+
+        const routeLineId = "routeline_" + routeStopElem.id.split("_")[1];
+
+        const stopElem = document.getElementById(routeLineId);
+
+
+        routeStopElem.addEventListener("mouseover", function (event) {
+            if (routeStopElem) {
+                stopElem.classList.add("show-route");
+                // move this line to the front
+                event.currentTarget.parentElement.append(event.currentTarget);
+            }
+            else {
+
+            }
+        });
+
 
     });
