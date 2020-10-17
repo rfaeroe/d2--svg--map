84cf0d30cbcc196863a4b870164afe03cf27cfe7
unknown <rune.faeroe@gmail.com>
2020-10-17 17:47:52 +0200

    completed MA1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
+
 document.querySelectorAll("[id^=routeline_]")
     .forEach(function (routeline) {
         routeline.addEventListener("mouseover", function (event) {
 
             // move this line to the front
             event.currentTarget.parentElement.append(event.currentTarget);
+            console.log(event.currentTarget);
         });
     });
 
 
 
-
-
 document.querySelectorAll('[id^="terminal_"]')
     .forEach(function (terminalElem) {
-        console.log(terminalElem.id, terminalElem)
+        console.log(terminalElem.id, terminalElem);
+
 
 
         // creat popup ID based on terminal ID
@@ -22,12 +23,16 @@ document.querySelectorAll('[id^="terminal_"]')
         // select the popup from the page
         const popupElem = document.getElementById(popupId);
 
+
+
         // set up hover events
         terminalElem.addEventListener("mouseover", function (event) {
             popupElem.classList.add("popup-open");
+            event.currentTarget.parentElement.append(event.currentTarget);
         });
         terminalElem.addEventListener("mouseout", function (event) {
             popupElem.classList.remove("popup-open");
+            event.currentTarget.parentElement.append(event.currentTarget);
         });
 
     });
@@ -35,28 +40,22 @@ document.querySelectorAll('[id^="terminal_"]')
 
 document.querySelectorAll('[id^="routestop_"]')
     .forEach(function (routeStopElem) {
-        routeStopElem.addEventListener("moseover", function (event) {
-            // move this line to the front
-            event.currentTarget.parentElement.append(event.currentTarget);
-        })
-
 
 
         const routeLineId = "routeline_" + routeStopElem.id.split("_")[1];
-
         const stopElem = document.getElementById(routeLineId);
 
-
         routeStopElem.addEventListener("mouseover", function (event) {
-            if (routeStopElem) {
-                stopElem.classList.add("show-route");
-                // move this line to the front
-                event.currentTarget.parentElement.append(event.currentTarget);
-            }
-            else {
+            stopElem.classList.add("show-route");
+            event.currentTarget.parentElement.append(event.currentTarget);
 
-            }
+        });
+
+        routeStopElem.addEventListener("mouseout", function (event) {
+            stopElem.classList.remove("show-route");
+
         });
 
 
     });
+
